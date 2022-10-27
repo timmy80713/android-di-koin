efbf0f70b85ffd4c18522e71193fbcc0f332b0ee
Timmy Chen <timmy80713@gmail.com>
2022-10-27 13:21:44 +0800

    feat: add koin annotation dependencies and some configuration.

---
+++ app/src/main/java/com/timmy/codelab/dikoin/injection/AppModule.kt
@@ -0,0 +1,8 @@
+package com.timmy.codelab.dikoin.injection
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.timmy.codelab.dikoin")
+class AppModule
